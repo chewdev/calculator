75d7c0206d59919a1389bcf4118844705c130583
Chris Ewald <itsbewald@gmail.com>
2022-01-20 22:52:33 -0800

    Add separate formatting for calculator screen text for input and output values

--- app.js
+++ app.js
@@ -75,51 +75,35 @@ const calculator = {
         this.getPrecision(this.previousVal),
         this.getPrecision(this.currentVal)
       );
-      let output = this.operate(
+      this.output = this.operate(
         this.currentOperator,
         Number(this.previousVal),
         Number(this.currentVal)
       );
-      const outputPrecision = this.getPrecision(output);
-      this.output =
-        precision && typeof output === "number"
-          ? output.toFixed(precision)
-          : outputPrecision && typeof output === "number"
-          ? output.toFixed(Math.min(outputPrecision, 4))
-          : output;
+      const outputPrecision = this.getPrecision(this.output);
       this.currentVal = "";
       this.previousVal = "";
       this.currentOperator = "";
+      this.showCurrentVal(precision, outputPrecision);
+    }
+  },
+  backspace: function () {
+    if (this.currentVal.length > 0) {
+      this.currentVal = this.currentVal.slice(0, -1);
       this.showCurrentVal();
     }
   },
-  showCurrentVal: function () {
-    let screenValue = (
-      this.output !== "" ? this.output : this.currentVal
-    ).toString();
-
-    if (screenValue.length > 13) {
-      const [int, dec] = screenValue.split(".");
-      if (int.length > 13) {
-        let exponent = int.length - 1;
-        let shortened = Number(`${int.slice(0, 1)}.${int.slice(1)}`).toFixed(2);
-        if (shortened.split(".")[0].length > 1) {
-          shortened = `${shortened.slice(0, 1)}.${shortened.slice(
-            1,
-            2
-          )}${shortened.slice(3, 4)}`;
-          exponent++;
-        }
-        screenValue = `${this.trimTrailing0Decimals(shortened)}e${exponent}`;
-      } else {
-        if (int.length > 0) {
-          screenValue = this.trimTrailing0Decimals(
-            `${int}.${dec.slice(0, 13 - int.length)}`
-          );
-        }
-      }
+  showCurrentVal: function (inputPrecision, outputPrecision) {
+    let screenValue;
+    if (this.output !== "") {
+      console.log(this.output);
+      screenValue = this.formatOutput(
+        this.output,
+        inputPrecision,
+        outputPrecision
+      );
     } else {
-      screenValue = this.trimTrailing0Decimals(screenValue);
+      screenValue = this.formatInput(this.currentVal);
     }
     calculatorScreen.textContent = screenValue;
   },
@@ -142,14 +126,64 @@ const calculator = {
     }
     return val;
   },
-  backspace: function () {
-    if (this.currentVal.length > 0) {
-      this.currentVal = this.currentVal.slice(0, -1);
-      this.showCurrentVal();
+  formatInput: function (input) {
+    if (input.toString().length <= 13) {
+      return input;
+    } else {
+      return this.formatTooLong(input);
+    }
+  },
+  formatOutput: function (output, inputPrecision, outputPrecision) {
+    console.log(typeof output);
+    if (typeof output !== "number") {
+      return output;
+    }
+    let formattedOutput = output.toString();
+    if (formattedOutput.includes(".")) {
+      let maxPrecision = 12;
+      let minPrecision = 3;
+      let userPrecision = Math.max(inputPrecision || 0, outputPrecision || 0);
+      let actualPrecision;
+      if (minPrecision > userPrecision) {
+        actualPrecision = minPrecision;
+      } else if (userPrecision > maxPrecision) {
+        actualPrecision = maxPrecision;
+      } else {
+        actualPrecision = userPrecision;
+      }
+      formattedOutput = Number(formattedOutput).toFixed(actualPrecision);
+    }
+    if (formattedOutput.length <= 13) {
+      return this.trimTrailing0Decimals(formattedOutput);
+    } else {
+      return this.trimTrailing0Decimals(this.formatTooLong(formattedOutput));
+    }
+  },
+  formatTooLong: function (value) {
+    let [int, dec] = value.split(".");
+    if (int.length > 13) {
+      let exponent = int.length - 1;
+      let shortened = Number(`${int.slice(0, 1)}.${int.slice(1)}`).toFixed(2);
+      if (shortened.split(".")[0].length > 1) {
+        shortened = `${shortened.slice(0, 1)}.${shortened.slice(
+          1,
+          2
+        )}${shortened.slice(3, 4)}`;
+        exponent++;
+      }
+      return `${this.trimTrailing0Decimals(shortened)}e${exponent}`;
+    } else {
+      let [base, exp] = value.split("e");
+      if (!exp) {
+        return value.slice(0, 13);
+      } else {
+        return this.trimTrailing0Decimals(Number(base).toFixed(2)) + `e${exp}`;
+      }
     }
   },
 };
 
+//button click listeners
 document.querySelectorAll("button").forEach((btn) => {
   const inputType =
     btn.attributes["inputtype"] && btn.attributes["inputtype"].value;
@@ -176,6 +210,7 @@ document.querySelectorAll("button").forEach((btn) => {
   }
 });
 
+//keystroke listeners
 document.body.addEventListener("keyup", (e) => {
   if (e.key.match(/[\d\.]/)) {
     calculator.updateCurrentVal(e.key);
